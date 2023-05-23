fc216bc2cc63e8456c143034c42eb0ca61ef4d00
Documenter.jl <documenter@juliadocs.github.io>
2023-05-23 16:12:21 +0000

    build based on b2b2d22

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MakieExtras","category":"page"},{"location":"#MakieExtras","page":"Home","title":"MakieExtras","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MakieExtras.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MakieExtras]","category":"page"}]
+}
